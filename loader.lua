336581919fcbbca86cdf10c8ddcf6f8d8f7201c1
triple7distro <triple7distro@proton.me>
2026-04-21 11:31:58 +0200

    asddasads

--- loader.lua
+++ loader.lua
@@ -1,7 +1,6 @@
 local repo = 'https://raw.githubusercontent.com/triple7distro/hexhook/main/'
-local supportedGameId = 3765739
 
-if game.CreatorId == supportedGameId then
+if game.CreatorId == 3765739 then
     loadstring(game:HttpGet(repo .. 'scripts/hexhook.lua'))()
 else
     game:GetService("Players").LocalPlayer:Kick("game not supported")
